189bbe04daf2d132bd4f0ac057ce9f2796028837
Tugce <tugceyalcin_07@hotmail.com>
2024-06-07 19:17:36 +0300

    update on vercel

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/authenticate", async (req, res) => {
+app.post("/api/authenticate", async (req, res) => {
     const { username } = req.body;
 
     try {
@@ -18,10 +18,8 @@ app.post("/authenticate", async (req, res) => {
         return res.status(r.status).json(r.data);
     } catch (e) {
         return res.status(e.response.status).json(e.response.data);
-
     }
-
     return res.json({ username: username, secret: "sha256..." });
 });
 
-app.listen(3001);
+module.exports = app;
